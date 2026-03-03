d8333820dc85e1fb7b0ef66ca2cbb1054a035c38
nikhilmandaofc-debug <nikhilmandaofc@gmail.com>
2026-03-03 15:37:01 -0600

    create

---
+++ database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLite database URL
+DATABASE_URL = "sqlite:///./test.db"
+
+# Create engine
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Session
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class
+Base = declarative_base()
+
+# User model
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
